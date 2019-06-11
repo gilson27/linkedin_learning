fb7f22ca6a7c399c7880e1674879dedb50dcf450
gilson27 <gilsonvarghese7@gmail.com>
2019-06-11 07:26:25 +0530

    Added routes

--- build_website_express/ch2/purenodeserver.js
+++ build_website_express/ch2/purenodeserver.js
@@ -6,11 +6,36 @@ const handler = (req, res) => {
     console.log(parsedURL);
     if (parsedURL.pathname === '/') {
         res.writeHead(200, {'Content-type':'text/plain'});
-        res.write('Hello I am a webserver');
+        res.write("Helllo World");
+        res.end();
+    } else if(parsedURL.pathname === '/hello') {
+        const name = parsedURL.query.name;
+        if (!name) {
+            res.writeHead(404, {'Content-type':'text/plain'});
+            res.end();
+            return;
+        }
+        res.writeHead(200, {'Content-type':'text/plain'});
+        res.write(`Hello ${name}`);
+        res.end();
+    } else if(parsedURL.pathname.startsWith('/user')) {
+        const regx = new RegExp('\/user\/(.+)');
+        const matches = regx.exec(parsedURL.pathname);
+        if (!matches || !matches[1]) {
+            res.writeHead(404, {'Content-type':'text/plain'});
+            res.end();
+            return;
+        }
+        res.writeHead(200, {'Content-type':'text/plain'});
+        res.write(`Hello ${matches[1]}`);
+        res.end();
+    } else if(parsedURL.pathname === '/time') {
+        res.writeHead(200, {'Content-type':'text/plain'});
+        res.write(new Date().toString());
         res.end();
     } else {
         res.writeHead(404, {'Content-type':'text/plain'});
-        res.end()
+        res.end();
     }
 
 };
