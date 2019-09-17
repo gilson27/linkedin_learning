e47ed4c9fb12d22b42662cc108deb698e706b515
gilson27 <delbiaambrose@gmail.com>
2019-09-17 20:24:21 +0530

    Completed python language features section

--- advanced_python/Exercise Files/01 Language/templstr_start.py
+++ advanced_python/Exercise Files/01 Language/templstr_start.py
@@ -1,5 +1,5 @@
 # demonstrate template string functions
-
+from string import Template
 
 def main():
     # Usual string formatting with format()
@@ -7,11 +7,17 @@ def main():
     print(str1)
     
     # TODO: create a template with placeholders
-    
+    templ = Template("You are watching ${title} by ${author}")
     # TODO: use the substitute method with keyword arguments
-    
+    str2= templ.substitute(title="AP",author="gilly")
+    print(str2)
     # TODO: use the substitute method with a dictionary
-
+    data  = {
+        "author": "gilly",
+        "title": "AP"
+    }
+    str3 = templ.substitute(data)
+    print(str3)
     
 if __name__ == "__main__":
     main()
