62dde8929a0cb40784586b6dc7fb643bfee13f0e
gilson27 <delbiaambrose@gmail.com>
2019-10-09 19:36:16 +0530

    Finished data structures

--- programming_foundation_algorithms/Exercise Files/2 Data Structures/Hash Table/hashtable_start.py
+++ programming_foundation_algorithms/Exercise Files/2 Data Structures/Hash Table/hashtable_start.py
@@ -2,15 +2,21 @@
 
 
 # TODO: create a hashtable all at once
-
-
+items1 = dict({"k1": 1, "k2": 2, "k3": "three"})
+print(items1)
 # TODO: create a hashtable progressively
-
-
+item2 = {}
+item2["k1"] = 1
+item2["k2"] = "two"
+item2["k3"] = 3
+print(item2)
 # TODO: try to access a nonexistent key
-
+# print(item2[3])
 
 # TODO: replace an item
-
+item2["k2"] = 56
+print(item2)
 
 # TODO: iterate the keys and values in the dictionary
+for k,v in item2.items():
+    print("Key ", k, ":Val ", v)
--- programming_foundation_algorithms/Exercise Files/2 Data Structures/Queues/queue_start.py
+++ programming_foundation_algorithms/Exercise Files/2 Data Structures/Queues/queue_start.py
@@ -1,13 +1,17 @@
 # try out the Python queue functions
-
+from collections import deque
 
 # TODO: create a new empty deque object that will function as a queue
-
+queue = deque()
 
 # TODO: add some items to the queue
-
-
+queue.append(11)
+queue.append(51)
+queue.append(30)
+queue.append(41)
 # TODO: print the queue contents
-
+print(queue)
 
 # TODO: pop an item off the front of the queue
+queue.popleft()
+print(queue)
